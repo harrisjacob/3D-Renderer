cc0ce12ed1e24d9d7b05296267c8bab0b1f7e547
Jacob Harris <harrisjakef@gmail.com>
2021-10-26 14:31:53 -0700

    Model.h initial commit

--- Basic-Renderer/include/model.h
+++ Basic-Renderer/include/model.h
@@ -1,8 +1,21 @@
 #ifndef __MODEL_H__
-#define
+#define __MODEL_H__
+
+#include "Geometry.h"
+#include <vector>
 
 
-
-
+class Model {
+private:
+	std::vector<Vec3f> vertices_;
+	std::vector<std::vector<int>> faces_;
+public:
+	Model(const char*);
+	int nVerts();
+	int nFaces();
+	Vec3f vert(int);
+	std::vector<int> face(int);
+	void debug();
+};
 
 #endif // __MODEL_H__
--- Basic-Renderer/src/model.cpp
+++ Basic-Renderer/src/model.cpp
@@ -1,1 +1,72 @@
-#include "model.h"
+#include "Model.h"
+
+#include <iostream> //std::cerr
+#include <sstream> //std::istringstream
+#include <fstream> //std::ifstream
+#include <string> //std::c_str
+
+
+//Constructor
+Model::Model(const char *filename) : vertices_(), faces_() {
+	std::ifstream in;
+	
+	//Open the OBJ file
+	in.open(filename, std::ifstream::in);
+	if(in.fail()) return;
+
+	std::string line;
+	while(!in.eof()){
+		std::getline(in, line);					//Get a single line from the input stream and read into string line
+		std::istringstream iss(line.c_str());	//Construct input stream from line to parse values (converts line to a C style string first)
+		char trash;								//Placeholder for reading values not needed
+		if(!line.compare(0, 2, "v ")){			//Compare the first two characters (is it a vertex?)
+			iss >> trash;						//Trash data type indicator(vertex)
+			//Read in a vector
+			Vec3f v;
+			iss >> v.x >> v.y >> v.z;
+			vertices_.push_back(v);
+		}else if(!line.compare(0, 2, "f ")){	//Is it a face?
+			std::vector<int> f;
+			iss>>trash;							//Trash the data type indicator(face)
+			int valTrash;						//TODO: Read in texture coordinates and normals (for now trash the vals)
+			int idx;						//Trash the slash character delimeters
+
+			//Format "vertexIdx/vertexTextureIdx/vertexNormalIdx ../../.. ../../.."
+			while(iss >> idx >> trash >> valTrash >> trash >> valTrash){
+				//Note each input stream has 3 vertices so this loop executes 3 times
+				idx--;				//Wavefront obj indexing starts at 1
+				f.push_back(idx);
+			}
+
+			faces_.push_back(f);
+		}
+
+		debug();
+
+	}
+}
+
+//Get the vertex count
+int Model::nVerts(){
+	return vertices_.size();
+}
+
+
+//Get the face count
+int Model::nFaces(){
+	return faces_.size();
+}
+
+//Get a vertex at index idx
+Vec3f Model::vert(int idx){
+	return vertices_[idx];
+}
+
+//Get a face at index idx
+std::vector<int> Model::face(int idx){
+	return faces_[idx];
+}
+
+void Model::debug(){
+	std::cerr << "#v: " << nVerts() << " #f " << nFaces() << "\n";
+}
